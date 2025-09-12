f2f01f262dcc3394174e57ab2cc70e26ccc297f2
peterleunam1 <arpm081001@gmail.com>
2025-09-12 09:03:32 -0500

    chore: :see_no_evil: add environment variables

--- src/app/infrastructure/email/email.ts
+++ src/app/infrastructure/email/email.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 import { SendEmailModel } from '../../domain/models/send-email';
 import { SendEmailGateway } from '../../domain/send-email/send-email.gateway';
+import { environment } from '../../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class SendEmailService extends SendEmailGateway {
-    private serviceId = 'service_zidfslc';
-  private templateId = 'template_scmpbpp';
-  private publicKey = '3vRTAR4-4Vj1oZh2b';
+  private serviceId = environment.SERVICE_ID;
+  private templateId = environment.TEMPLATE_ID;
+  private publicKey = environment.PUBLIC_KEY;
 
   sendEmail(params: SendEmailModel): Promise<EmailJSResponseStatus> {
     return emailjs.send(this.serviceId, this.templateId, params, this.publicKey);
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  SERVICE_ID: 'service_zidfslc',
+  TEMPLATE_ID: 'template_scmpbpp',
+  PUBLIC_KEY: '3vRTAR4-4Vj1oZh2b'
+};
