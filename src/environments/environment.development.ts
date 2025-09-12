f9aea46611bacb55ec0d721758e7541b5b3cfe05
peterleunam1 <arpm081001@gmail.com>
2025-09-12 10:18:37 -0500

    feat: :art: change favicon

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
-export const environmentDev = {
+export const environment = {
   SERVICE_ID: 'service_zidfslc',
   TEMPLATE_ID: 'template_scmpbpp',
   PUBLIC_KEY: '3vRTAR4-4Vj1oZh2b'
--- src/environments/index.ts
+++ src/environments/index.ts
@@ -1,2 +1,1 @@
-export * from './environment.development';
 export * from './environment';
